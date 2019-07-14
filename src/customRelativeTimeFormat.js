5041d70576ad4d5c6d90f197fe8f27ccdc20b57b
lvzhenbang <lvzhenbang@outlook.com>
2019-07-14 21:29:23 +0800

    fix: format bug

--- src/customRelativeTimeFormat.js
+++ src/customRelativeTimeFormat.js
@@ -2,7 +2,7 @@ import langMap from './config/lang';
 
 class CustomRelativeTimeFormat {
   consturctor(lang) {
-    this.local = langMap[lang] ? lang : this.local;
+    this.local = langMap[lang] ? lang : 'en';
     this.localData = langMap[this.local];
     this.init();
   }
@@ -13,28 +13,46 @@ class CustomRelativeTimeFormat {
 
   format(value, unit) {
     let outputStr = 'invalid date.';
-    if (value > 0) {
-      if (this.localData[unit].future.one) {
-        if (value === 1) outputStr = this.localData[unit].future.one.replace(/\{0\}/, Math.abs(value));
-        outputStr = this.localData[unit].future.other.replace(/\{0\}/, Math.abs(value));
-      } else {
-        if (value === 1 && this.local === 'zh') outputStr = this.localData[unit].previous.replace(/\{0\}/, Math.abs(value));
-        if (value === 2 && this.local === 'zh') outputStr = this.localData[unit].previous2.replace(/\{0\}/, Math.abs(value));
-        outputStr = this.localData[unit].future.replace(/\{0\}/, Math.abs(value));
-      }
-    } else if (value === 0) {
-      outputStr = this.localData[unit].current.replace(/\{0\}/, Math.abs(value));
-    } else if (value < 0) {
-      if (this.localData[unit].past.one) {
-        if (value === -1) outputStr = this.localData[unit].past.one.replace(/\{0\}/, Math.abs(value));
-        outputStr = this.localData[unit].past.other.replace(/\{0\}/, Math.abs(value));
-      } else {
-        if (value === -1 && this.local === 'zh') outputStr = this.localData[unit].next.replace(/\{0\}/, Math.abs(value));
-        if (value === -2 && this.local === 'zh') outputStr = this.localData[unit].next2.replace(/\{0\}/, Math.abs(value));
-        outputStr = this.localData[unit].past.replace(/\{0\}/, Math.abs(value));
-      }
+    switch (unit) {
+      case 'year':
+      case 'month':
+      case 'day':
+        if (value > 1) {
+          if (this.local === 'zh' && this.localData[unit]['next2']) {
+            outputStr = this.localData[unit]['next2'].replace(/\{0\}/, Math.abs(value));
+          } else {
+            outputStr = this.localData[unit].future.replace(/\{0\}/, Math.abs(value));
+          }
+        } else if (value === 1) {
+          outputStr = this.localData[unit].next.replace(/\{0\}/, Math.abs(value));
+        } else if (value === 0) {
+          outputStr = this.localData[unit].current.replace(/\{0\}/, Math.abs(value));
+        } else if (value === -1) {
+            outputStr = this.localData[unit].previous.replace(/\{0\}/, Math.abs(value));
+        } else {
+          if (this.local === 'zh' && this.localData[unit]['previous2']) {
+            outputStr = this.localData[unit]['previous2'].replace(/\{0\}/, Math.abs(value));
+          } else {
+            outputStr = this.localData[unit].past.replace(/\{0\}/, Math.abs(value));
+          }
+        }
+        break;
+      case 'hour':
+      case 'minute':
+      case 'second':
+        if (value > 1) {
+          outputStr = this.localData[unit].future.replace(/\{0\}/, Math.abs(value));
+        } else if (value === 1) {
+          outputStr = this.localData[unit].next.replace(/\{0\}/, Math.abs(value));
+        } else if (value === 0) {
+          outputStr = this.localData[unit].current.replace(/\{0\}/, Math.abs(value));
+        } else if (value === -1) {
+            outputStr = this.localData[unit].previous.replace(/\{0\}/, Math.abs(value));
+        } else {
+          outputStr = this.localData[unit].past.replace(/\{0\}/, Math.abs(value));
+        }
     }
-
+    
     return outputStr;
   }
 }
